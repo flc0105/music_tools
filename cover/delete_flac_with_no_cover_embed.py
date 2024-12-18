332f3143f0160940901f13b187ef1ca9c0c0826a
fangliangchen <fangliangchen@protonmail.com>
2024-12-18 13:24:11 +0800

    fix

--- cover/delete_flac_with_no_cover_embed.py
+++ cover/delete_flac_with_no_cover_embed.py
@@ -2,22 +2,21 @@ import os
 from mutagen.flac import FLAC
 from pathlib import Path
 
-def delete(flac_path):
-    try:
-        audio = FLAC(flac_path)
-        pictures = audio.pictures
-        if len(pictures) == 0:
-            os.remove(flac_path)
-        print(f"已删除: {flac_path}")
-    except Exception as e:
-        print(f"删除失败 {flac_path}: {e}")
+
 
 def process_folder(folder_path):
     folder = Path(folder_path)
     flac_files = list(folder.glob('*.flac'))
 
-    if flac_files:
-        delete(flac_files[0])
+   for flac_file in flac_files:
+        try:
+            audio = FLAC(flac_file)
+            pictures = audio.pictures
+            if len(pictures) == 0:
+                os.remove(flac_file)
+            print(f"已删除: {flac_file}")
+        except Exception as e:
+            print(f"删除失败 {flac_file}: {e}")
 
     for subfolder in folder.iterdir():
         if subfolder.is_dir():
